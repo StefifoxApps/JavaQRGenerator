56be6db06c27592be8c8d96c30fe5a786045b914
Stefano <30950154+Stefifox@users.noreply.github.com>
2019-09-28 21:17:38 +0200

    Updated Javadoc

--- Javadoc/type-search-index.js
+++ Javadoc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"dev.stefifox.qr","l":"Frame"},{"p":"dev.stefifox.qr","l":"LoadSettings"},{"p":"dev.stefifox.qr","l":"Main"},{"p":"dev.stefifox.qr","l":"QR_Generator"},{"p":"dev.stefifox.qr.general","l":"QrCode"},{"p":"dev.stefifox.qr","l":"SaveSettings"},{"p":"dev.stefifox.qr","l":"Settings"},{"p":"dev.stefifox.qr","l":"TextInput"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"dev.stefifox.qr.general","l":"QrCode"}]
