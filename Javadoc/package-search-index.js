7373b9ee71d0650e2beaf3e06708bbb1f05054f8
Stefano <30950154+Stefifox@users.noreply.github.com>
2019-09-28 20:47:09 +0200

    Updated Javadoc

--- Javadoc/package-search-index.js
+++ Javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"dev.stefifox.qr"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"dev.stefifox.qr"},{"l":"dev.stefifox.qr.general"}]
